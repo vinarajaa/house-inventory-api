5ebde25ab8bf09458b6e3b0036d47753524178b5
vinarajaa <vinaraja01@yahoo.com>
2025-05-13 15:33:07 -0700

    Fix error in database.py

--- app/database.py
+++ app/database.py
@@ -7,8 +7,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+# DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+# engine = create_engine(DATABASE_URL)
+# SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+# Base = declarative_base()
